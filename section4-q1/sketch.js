4535c7736c8f6213913c9368e2865eed2b8cc845
MT-257 <2280893h@cloud.kobe-u.jp>
2023-10-24 14:00:18 +0900

    change 3-3

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
 
   // 横線を引く
@@ -15,10 +15,15 @@ function setup(){
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
 
   // ここからが本番
+  function  i(arr){
   fill(0);
   const dx = width / scores.length;
   let px, py; // 線を引くために一つ前の点を覚えておく変数
-  for(let i = 0; i < scores.length; i++){
+  beginShape();
+  for(let i = 0; i < scores.length; i++){vertex(px,py)
     // BLANK[1]
+
   }
+  endShape(CLOSE)
+}
 }
